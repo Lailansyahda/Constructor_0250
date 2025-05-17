49ad67af85d91413a6bda3ed272c42f5b3476643
Lailansyahda <syahdaazalia123@gmail.com>
2025-05-17 14:40:00 +0700

    function member definition

--- destruktor.cpp
+++ destruktor.cpp
@@ -12,3 +12,10 @@ class angka{
   void cetakData();
   void isiData();
 };
+
+//Definisi member Function
+angka::angka(int i){ //Constructor
+    panjang = i;
+    arr = new int[i];
+    isiData();
+}
