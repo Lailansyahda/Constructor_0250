583336f26ae0c11d233e4baf84e7a301e970e87a
Lailansyahda <syahdaazalia123@gmail.com>
2025-05-17 14:27:16 +0700

    calling chain function

--- Constructor.cpp
+++ Constructor.cpp
@@ -12,4 +12,16 @@ class buku
             this->judul = judul;
             return *this; //chain function
         }
+        string getJudul()
+        {
+            return this->judul;
+        }
+} bukunya;
+
+int main()
+{
+    //bukunya.setJudul("Matematika");
+    //cout << bukunya.getJudul();
+    cout << bukunya.setJudul("Matematika").getJudul(); //chain function calls
+    return 0;
 }
