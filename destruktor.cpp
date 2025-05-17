f7a4741d105cf58916c254d784748624fd99fa66
Lailansyahda <syahdaazalia123@gmail.com>
2025-05-17 14:48:39 +0700

    prosedur angka isi data

--- destruktor.cpp
+++ destruktor.cpp
@@ -29,6 +29,13 @@ angka::~angka(){//Destructor
 
 void angka::cetakData(){
     for(int i=1;i<=panjang;i++){
-            cout<<i<<" = "<<arr[i]<<endl;
+        cout<<i<<" = "<<arr[i]<<endl;
     }
+}
+
+void angka::isiData(){
+    for(int i=1;i<=panjang;i++){
+        cout<<i<<" = ";cin>>arr[i];
+    }
+    cout<<endl;
 }
